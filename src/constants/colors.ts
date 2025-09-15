07a9a2f432be277931c96b2d22c96a00845e1703
Yoomin Hwang <yoominh3@gmail.com>
2025-09-15 15:24:45 +0900

    fix: color system 반영

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,16 +1,19 @@
 const Colors = {
   // 피그마 색상 시스템 그대로 가져왔습니다!
+  // 1, 2, 3, 4 색상
   primary: '#4052E2',
   secondary: '#3589F8',
   tertiary: '#EBF4FD',
   quartenary: '#F2F6FC',
 
+  // 일반 색상
   mainwhite: '#FFFFFF',
   black: '#000000',
   bluegray1: '#9199A6',
   gray1: '#626262',
   pink: '#FFE3E3',
 
+  // 컴포넌트 색상
   bg: '#F9FBFF',
   button: '#D5D5D5',
 };
