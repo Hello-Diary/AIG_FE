1704415eca28a9516dff64713383243e8bec09a7
Yoomin Hwang <yoominh3@gmail.com>
2025-10-03 10:00:06 +0900

    hotfix: all merge conflict solved

--- app/(tabs)/diary/index.tsx
+++ app/(tabs)/diary/index.tsx
@@ -1,26 +1,6 @@
 import React from 'react';
 import DiaryScreen from './writeDiary';
 
-<<<<<<< HEAD
-export default function DiaryScreen() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Diary Write Page</Text>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    gap: 20,
-    paddingHorizontal: 20,
-    marginTop: 10,
-    backgroundColor: c.bg,
-  },
-});
-=======
 export default function DiaryPage() {
   return <DiaryScreen />;
 }
->>>>>>> f28377c6f5e7d039e34d0ae9be8e2effb78810a6
