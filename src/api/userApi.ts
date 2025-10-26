8c48cd005ebd753a267de6b2a2bf06b809cfc814
Yoomin Hwang <yoominh3@gmail.com>
2025-10-27 07:42:56 +0900

    hotfix: 바뀐 명세서에 맞게 user  api 살짝 수정

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,14 +1,14 @@
 import { ENDPOINT } from "./endPoint";
 import { http } from "./http";
 
-import { UserRequest, UserResponse } from "../types/user";
+import { UserPatchRequest, UserResponse } from "../types/user";
 
 export const getUserDataApi = async (userId: string) => {
   const res = await http.get<UserResponse>(`${ENDPOINT.USER}/${userId}`);
   return res;
 };
 
-export const patchUserDataApi = async (userId: string, data: UserRequest) => {
+export const patchUserDataApi = async (userId: string, data: UserPatchRequest) => {
   const res = await http.patch(`${ENDPOINT.USER}/${userId}`, data);
   return res;
 };
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,4 +1,4 @@
-export interface UserRequest {
+export interface UserPatchRequest {
   name: string;
   profile: string;
 }
